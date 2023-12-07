8b518dc61859180d59c6fbd130f605bf618cc91a
Zi Liang <zi.liang@edu.sait.ca>
2023-12-06 22:14:23 -0700

    Added apptReports.py code

--- Project JoJo's Salon/Project1Part2/apptReports.py
+++ Project JoJo's Salon/Project1Part2/apptReports.py
@@ -1,7 +1,35 @@
 import appointment as ap
 def show_appointments_by_name(appt_cal: list[ap.Appointment], client_name):
-    pass
+    found_appts = []
+    for appt in appt_cal:
+        if appt.get_client_name().lower().startswith(client_name.lower()):
+            found_appts.append(appt)
+
+    print("{:20s}{:15s}{:10s}{:10s}{:10s}{:20s}".format("Client Name",
+        "Phone", "Day", "Start", "End", "Type"))
+    print("-" * 85)
+    
+    if len(found_appts) == 0:
+        print("No appointments found.")
+        return False
+    
+    for appt in found_appts:
+        print(appt)
+
+    return True
 
 def show_appointments_by_day(appt_cal: list[ap.Appointment], day_of_week):
     # show_appointments_by_day - prints a report of all appointments for a specific day of the week
-    pass
+    found_appts = []
+    for appt in appt_cal:
+        if appt.get_day_of_week().lower() == day_of_week.lower():
+            found_appts.append(appt)
+
+    print("{:20s}{:15s}{:10s}{:10s}{:10s}{:20s}".format("Client Name",
+        "Phone", "Day", "Start", "End", "Type"))
+    print("-" * 85)
+    
+    for appt in found_appts:
+        print(appt)
+
+    return True
