2f1397e78814ac360380ca281bbfd8c312bc9b0a
jvcha <jvchavda2755@gmail.com>
2023-12-07 11:35:46 -0700

    Final: Commented, Formatted made some  changes

--- Project JoJo's Salon/Project1Part2/appointment.py
+++ Project JoJo's Salon/Project1Part2/appointment.py
@@ -6,25 +6,24 @@ class Appointment:
     
     # constructor for the Appointment class
     def __init__(self, day_of_week, start_time_hour):
-        self.__day = day_of_week
-        self.__hour = start_time_hour
-        self.__type = 0
-        self.__nameclient = ""
-        self.__phoneclient = ""
+        self.__name = ""        #initializing empty string for client name
+        self.__phone = ""       #intializing empty string for client phone
+        self.__type = 0         #intializing integer 0 for appointment type
+        self.__day = day_of_week    # Day of the week when the appointment is scheduled
+        self.__hour = start_time_hour   # Hour when the appointment starts
 
-    # getter and setters for the attributes of an Appointment object
-
+    # Methods to get clients name, phone, and appointment
     def get_client_name(self):
-        return self.__nameclient
+        return self.__name
 
     def set_client_name(self, client_name):
-        self.__nameclient = client_name
+        self.__name = client_name
 
     def get_client_phone(self):
-        return self.__phoneclient  
+        return self.__phone
     
     def set_client_phone(self, client_phone):
-        self.__phoneclient = client_phone
+        self.__phone = client_phone
 
     def get_appt_type(self):
         return self.__type
@@ -32,6 +31,7 @@ class Appointment:
     def set_appt_type(self, appt_type):
         self.__type = appt_type
 
+    # Methods to get the appointment day and time
     def get_day_of_week(self):
         return self.__day
 
@@ -40,7 +40,8 @@ class Appointment:
     
     def get_end_time_hour(self):
         return self.__hour + 1
-        
+
+    # Methods to schedule and cancel the appointment
     def schedule(self, client_name, client_phone, appt_type):
         self.set_client_name(client_name)
         self.set_client_phone(client_phone)
@@ -51,11 +52,14 @@ class Appointment:
         self.set_client_phone("")
         self.set_appt_type(0)
 
+    # Method to get the appointment type description
     def get_appt_type_desc(self):
         return Appointment.APPT_TYPE_DESCS[self.__type]
     
+    # Method to format the appointment record as string
     def format_record(self):
-        return f"{self.__nameclient},{self.__phoneclient},{self.__type},{self.__day},{self.__hour:02d}"
+        return f"{self.__name},{self.__phone},{self.__type},{self.__day},{self.__hour:02d}"
     
+    # Method to format the appointment details as a string
     def __str__(self):
-        return f"{self.__nameclient:<20}{self.__phoneclient:<15}{self.__day:<10}{self.__hour:02d}:00  -  {self.get_end_time_hour():02d}:00     {self.get_appt_type_desc()}"
+        return f"{self.__name:<20}{self.__phone:<15}{self.__day:<10}{self.__hour:02d}:00  -  {self.get_end_time_hour():02d}:00     {self.get_appt_type_desc()}"
